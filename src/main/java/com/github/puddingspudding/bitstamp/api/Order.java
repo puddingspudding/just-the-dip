e8c52cf2e2da464c67d57e6fa68953d909367302
Philip Strecker <philip.strecker@googlemail.com>
2022-06-19 14:46:25 +0200

    updated order min value

--- src/main/java/com/github/puddingspudding/bitstamp/api/Order.java
+++ src/main/java/com/github/puddingspudding/bitstamp/api/Order.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 
 public class Order {
 
-    public static double MIN_VALUE = 20.0;
+    public static double MIN_VALUE = 10.0;
 
     private long id;
     private String datetime;
